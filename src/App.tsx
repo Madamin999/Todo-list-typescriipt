8fbe92aa8dbf2d71f8a27ef8b0fca63422953be4
Madamin999 <madamink555@gmail.com>
2022-11-18 23:26:49 +0600

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App flex justify-center items-center">
+        <div className="mb-6">
+            <label htmlFor="success" className="block my-7 text-5xl font-medium text-green-700 dark:text-green-500 ">TODO LIST</label>
+           <div className="flex">
+               <input type="text" id="success"
+                      className="bg-green-50 border border-green-500 text-green-900 dark:text-green-400 placeholder-green-700
+                   dark:placeholder-green-500 text-sm
+                   rounded-lg focus:ring-green-500 focus:border-green-500 block  p-2.5 w-[500px]
+                   dark:bg-gray-700 dark:border-green-500 mx-5"
+                      placeholder="Success input"/>
+               <button
+                   className="text-white right-2.5 bottom-2.5 bg-green-900 hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Search
+               </button>
+           </div>
+                <p className="mt-2 text-sm text-green-600 dark:text-green-500"><span
+                    className="font-medium">Well done!</span> Some success messsage.</p>
+        </div>
     </div>
   );
 }
